0b7794c5a5132e5e268de9e93cfbd82e393c19a4
rzippo <raffaele.zippo@live.it>
2018-05-31 16:25:17 +0200

    Add info dialog

--- Mdf2IsoUWP/Mdf2IsoUWP/MainPage.xaml.cs
+++ Mdf2IsoUWP/Mdf2IsoUWP/MainPage.xaml.cs
@@ -2,6 +2,7 @@
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Runtime.InteropServices.ComTypes;
 using System.Runtime.InteropServices.WindowsRuntime;
 using System.Threading;
 using System.Threading.Tasks;
@@ -101,6 +102,12 @@ namespace Mdf2IsoUWP
 
                 CancelButton.IsEnabled = false;
             }
+        }
+
+        private async void Info_OnClick(object sender, RoutedEventArgs e)
+        {
+            var infoDialog = new InfoDialog();
+            await infoDialog.ShowAsync();
         }
     }
 }
